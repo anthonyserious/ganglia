84d2b96f27c72aaccc05f406e39e7faa74a0e033
Anthony Schneider <anthony.schneider@electronifie.com>
2015-11-04 15:16:02 -0500

    Fix cross.sh example.

--- examples/cross.sh
+++ examples/cross.sh
@@ -2,14 +2,14 @@
 
 #
 #  In this example, we create the network, then call /api/networiks/cross/train with training data, as opposed
-#  to first adding training data (/api/networks/cross/trainingdata), then trainng the network.
+#  to first adding training data (/api/network/cross/trainingdata), then trainng the network.
 #
 
 echo "Creating cross network"
-curl -XPOST -H "Content-Type: application/json" -d '{"options":{"layers":[25]}}' http://localhost:8182/api/networks/cross
+curl -XPOST -H "Content-Type: application/json" -d '{"options":{"layers":[25]}}' http://localhost:8182/api/network/cross
 echo
 echo "Creating/training cross network"
-curl -XPOST -H "Content-Type: application/json" -d @`dirname $0`/cross.json http://localhost:8182/api/networks/cross/train
+curl -XPOST -H "Content-Type: application/json" -d @`dirname $0`/cross.json http://localhost:8182/api/network/cross/train
 echo
 echo "Running with a cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -17,7 +17,7 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   0,0,0,1,0, 
   1,1,1,1,1, 
   0,0,0,1,0, 
-  0,0,0,1,0]}' http://localhost:8182/api/networks/cross/run
+  0,0,0,1,0]}' http://localhost:8182/api/network/cross/run
 echo
 echo "Running with a cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -25,7 +25,7 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   0,0,1,1,1, 
   0,0,0,1,0, 
   1,1,0,1,0, 
-  0,0,0,0,0]}' http://localhost:8182/api/networks/cross/run
+  0,0,0,0,0]}' http://localhost:8182/api/network/cross/run
 echo
 echo "Running with a cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -33,7 +33,7 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   0,0,1,1,1, 
   0,0,0,1,0, 
   0,0,0,1,0, 
-  0,0,0,0,0]}' http://localhost:8182/api/networks/cross/run
+  0,0,0,0,0]}' http://localhost:8182/api/network/cross/run
 echo
 echo "Running with a cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -41,7 +41,7 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   0,1,0,0,0, 
   0,1,0,0,0, 
   1,1,1,0,0, 
-  0,1,0,0,0]}' http://localhost:8182/api/networks/cross/run
+  0,1,0,0,0]}' http://localhost:8182/api/network/cross/run
 echo
 echo "Running with a non-cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -49,7 +49,7 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   0,0,0,1,0, 
   0,0,0,1,0, 
   1,0,0,1,1, 
-  0,0,0,0,0]}' http://localhost:8182/api/networks/cross/run
+  0,0,0,0,0]}' http://localhost:8182/api/network/cross/run
 echo
 echo "Running with a non-cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -57,7 +57,7 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   0,1,0,0,0, 
   0,1,0,0,0, 
   0,1,1,1,0, 
-  0,1,0,0,0]}' http://localhost:8182/api/networks/cross/run
+  0,1,0,0,0]}' http://localhost:8182/api/network/cross/run
 echo
 echo "Running with a non-cross"
 curl -XPOST -H "Content-Type: application/json" -d '{"data":[
@@ -65,6 +65,6 @@ curl -XPOST -H "Content-Type: application/json" -d '{"data":[
   1,0,1,0,0, 
   1,0,1,1,1, 
   1,0,1,0,0, 
-  0,0,0,0,0]}' http://localhost:8182/api/networks/cross/run
+  0,0,0,0,0]}' http://localhost:8182/api/network/cross/run
 echo
 
